e0f456218db2e34eae76e0f2fc5f6d07ade65154
alfa-rsa <alfasanene@gmail.com>
2026-04-13 18:04:22 +0200

    feat: scaffold etenders project

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+export const BASE_URL = 'https://ocds-api.etenders.gov.za';
+export const DEFAULT_PAGE_SIZE = 50;
+export const DEFAULT_MAX_PAGES = 5;
+export const REQUEST_TIMEOUT_MS = 10000;
